d7b97bf662787be40945895b381c7db8e62006b5
Ashley Sullins & Juliana Suzuki <student@epicodus.com>
2015-10-29 14:00:29 -0700

    create orders and display total price

---
+++ app/src/main/java/com/epicodus/postmatesclone/models/Order.java
@@ -0,0 +1,53 @@
+package com.epicodus.postmatesclone.models;
+
+import com.activeandroid.Model;
+import com.activeandroid.annotation.Column;
+import com.activeandroid.annotation.Table;
+import com.activeandroid.query.Select;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Created by Guest on 10/29/15.
+ */
+@Table(name = "Orders", id = "_id")
+public class Order extends Model {
+
+    @Column(name = "Product")
+    private Product mProduct;
+
+    public Product getProduct() {
+        return mProduct;
+    }
+
+    public Order() {
+        super();
+    }
+
+    public Order(Product product) {
+        super();
+        mProduct = product;
+    }
+
+    public static List<Order> getOrder() {
+        return new Select()
+            .from(Order.class)
+            .execute();
+    }
+
+    public static List<Product> getProducts(){
+        List<Order> allOrders;
+        allOrders = new ArrayList<>();
+        allOrders = Order.getOrder();
+
+        List<Product> allProducts;
+        allProducts = new ArrayList<>();
+        for (Order order : allOrders){
+            allProducts.add(order.getProduct());
+        }
+        return allProducts;
+    }
+
+
+ }
--- app/src/main/java/com/epicodus/postmatesclone/ui/CheckoutActivity.java
+++ app/src/main/java/com/epicodus/postmatesclone/ui/CheckoutActivity.java
@@ -7,30 +7,55 @@ import android.view.MenuItem;
 import android.widget.TextView;
 
 import com.epicodus.postmatesclone.R;
+import com.epicodus.postmatesclone.models.Order;
 import com.epicodus.postmatesclone.models.Product;
 
+import org.w3c.dom.Text;
+
 import java.util.ArrayList;
+import java.util.List;
 
 public class CheckoutActivity extends AppCompatActivity {
 
-    private ArrayList<Product> mOrder;
+    //private ArrayList<Product> mOrder;
     private Product mProduct;
     private TextView displayProductName;
+    private TextView mTotalPrice;
+    //private ArrayList<Product> mAllProducts;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_checkout);
 
+        //ArrayList<Product> mOrder = new ArrayList<>();
+
         String product = getIntent().getStringExtra("product");
-        //mProduct = Product.find(product);
+        mProduct = Product.find(product);
         //mOrder.add(mProduct);
+
+        Order order = new Order(mProduct);
+        order.save();
 
         displayProductName = (TextView) findViewById(R.id.displayProduct);
         displayProductName.setText(product);
 
+        mTotalPrice = (TextView) findViewById(R.id.totalPrice);
+        mTotalPrice.setText(getTotal() + "");
 
+    }
 
+    public int getTotal(){
+        List<Product> productList = new ArrayList<>();
+        productList = Order.getProducts();
+        int firstPrice = productList.get(0).getPrice();
+        Integer totalPrice = 0;
+
+        for(Product product : productList){
+            totalPrice += product.getPrice();
+        }
+        return totalPrice;
     }
 
 
