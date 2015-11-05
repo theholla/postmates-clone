67b16bfa18bd5b0c472439e6638677044a3df547
holland & sullins <student@epicodus.com>
2015-11-04 16:54:38 -0800

    end of day, add User model, Begin integrating login and register functionality

---
+++ app/src/main/java/com/epicodus/postmatesclone/models/User.java
@@ -0,0 +1,29 @@
+package com.epicodus.postmatesclone.models;
+
+
+public class User {
+    private String mName;
+    private String mPassword;
+
+    public User() {
+        super();
+    }
+
+    public User(String name, String password) {
+        super();
+        mName = name;
+        mPassword = password;
+    }
+
+    public String getName() {
+        return mName;
+    }
+
+    public String getPassword() {
+        return mPassword;
+    }
+
+    public static User find(String username, String password) {
+        //query parse
+    }
+}
--- app/src/main/java/com/epicodus/postmatesclone/ui/MainActivity.java
+++ app/src/main/java/com/epicodus/postmatesclone/ui/MainActivity.java
@@ -2,6 +2,7 @@ package com.epicodus.postmatesclone.ui;
 
 import android.app.AlertDialog;
 import android.content.Intent;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -14,6 +15,7 @@ import com.facebook.stetho.Stetho;
 import com.parse.LogInCallback;
 import com.parse.ParseException;
 import com.parse.ParseUser;
+import com.parse.SignUpCallback;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -33,32 +35,30 @@ public class MainActivity extends AppCompatActivity {
         mUsernameInput = (EditText) findViewById(R.id.newUsername);
         mPasswordInput = (EditText) findViewById(R.id.newPassword);
         mLoginButton = (Button) findViewById(R.id.btnLogin);
-        mRegisterButton = (Button) findViewById(R.id.btnRegisterPage);
-        //mCompanyLoginButton = (Button) findViewById(R.id.companyLoginButton);
-        mResetPassword = (TextView) findViewById(R.id.txtResetPassword);
-
-        mRegisterButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent intent = new Intent(MainActivity.this, RegisterActivity.class);
-                startActivity(intent);
-            }
-        });
 
         mLoginButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                String username = mUsernameInput.getText().toString();
-                String password = mPasswordInput.getText().toString();
+                final String username = mUsernameInput.getText().toString();
+                final String password = mPasswordInput.getText().toString();
+
+                // TODO: Make model for user, password should check if name and pw match to let them log in.
+
                 ParseUser.logInInBackground(username, password, new LogInCallback() {
                     @Override
                     public void done(ParseUser user, ParseException e) {
                         if (user != null) {
-                            Intent intent = new Intent(MainActivity.this, StoreActivity.class);
-                            intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
-                            intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK);
-                            startActivity(intent);
-                        } else {
+                            goToMainPage();
+                        }
+                        else if (user == null) {
+                            String username = mUsernameInput.getText().toString().trim();
+                            String password = mPasswordInput.getText().toString().trim();
+                            String role = "customer";
+                            registerUser(username, password, role);
+                            goToMainPage();
+                        }
+                        //TODO: Add error handling if username and password do not match
+                         else {
                             AlertDialog show = new AlertDialog.Builder(MainActivity.this)
                                     .setTitle("Message")
                                     .setMessage("Error: username or password wrong")
@@ -70,4 +70,52 @@ public class MainActivity extends AppCompatActivity {
                 }
         });
     }
+
+    private void goToMainPage() {
+        Intent intent = new Intent(MainActivity.this, StoreActivity.class);
+        intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+        intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK);
+        startActivity(intent);
+    }
+
+    private void registerUser(String username, String password, String role) {
+        if (username.isEmpty() || password.isEmpty()) {
+            showErrorDialog();
+        } else {
+            ParseUser newUser = getParseUser(username, password);
+            newUser.put("role", role);
+
+            newUser.signUpInBackground(new SignUpCallback() {
+                @Override
+                public void done(ParseException e) {
+                    setProgressBarIndeterminateVisibility(false);
+
+                    if (e == null) {
+                        // Success!
+                        goToMainPage();
+                    } else showErrorDialog();
+                }
+            });
+        }
+    }
+
+    @NonNull
+    private ParseUser getParseUser(String username, String password) {
+        setProgressBarIndeterminateVisibility(true);
+
+        ParseUser newUser = new ParseUser();
+        newUser.setUsername(username);
+        newUser.setPassword(password);
+        return newUser;
+    }
+
+    private void showErrorDialog() {
+        AlertDialog.Builder builder = new AlertDialog.Builder(MainActivity.this);
+        builder.setMessage("This registration won't work for us.")
+                .setTitle("Oops!")
+                .setPositiveButton(android.R.string.ok, null)
+                .create()
+                .show();
+    }
 }
+
